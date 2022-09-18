8bb25e6e587f1396f0de634d9c867a28b5c043bb
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2022-09-18 20:07:19 +0700

    Finish - TailwindCSS installation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,9 @@
 import '../styles/globals.css'
+import "@fontsource/poppins/400.css"
+import "@fontsource/poppins/600.css"
+import "@fontsource/poppins/700.css"
+import "@fontsource/merriweather/400.css"
+import "@fontsource/merriweather/700.css"
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
