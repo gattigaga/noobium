38955fd49d8a30b613e5a52584675cfdf8c15f4b
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2022-09-22 00:09:42 +0700

    Finish - Slicing Home page

---
+++ components/AccountDropdown.tsx
@@ -0,0 +1,45 @@
+import Link from "next/link";
+
+type Props = {};
+
+const AccountDropdown: React.FC<Props> = ({}) => {
+  const user = {
+    name: "John Doe",
+    email: "john.doe@gmail.com",
+  };
+
+  return (
+    <div className="w-48 rounded-md border border-slate-200 absolute bg-white right-0 top-full">
+      <div className="p-4 border-b border-slate-200">
+        <p className="font-sans text-slate-900 font-bold">{user.name}</p>
+        <p className="font-sans text-slate-400 text-xs">{user.email}</p>
+      </div>
+
+      <div className="p-4">
+        <ul>
+          <li className="mb-3">
+            <Link href="#">
+              <a>
+                <p className="font-sans text-slate-900 text-xs">My Profile</p>
+              </a>
+            </Link>
+          </li>
+          <li className="mb-3">
+            <Link href="#">
+              <a>
+                <p className="font-sans text-slate-900 text-xs">My Articles</p>
+              </a>
+            </Link>
+          </li>
+          <li>
+            <button className="h-4 font-sans text-red-500 text-xs" type="button">
+              Sign Out
+            </button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default AccountDropdown;
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,10 +1,18 @@
 import Link from "next/link";
+import { useState } from "react";
+import { MdSearch } from "react-icons/md";
 
 import Button from "./Button";
+import AccountDropdown from "./AccountDropdown";
 
 type Props = {};
 
 const NavBar: React.FC<Props> = () => {
+  const [keyword, setKeyword] = useState("");
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const isLoggedIn = true;
+
   return (
     <header className="h-16 border-b border-slate-200 flex items-center justify-between px-24">
       <Link href="/">
@@ -13,11 +21,37 @@ const NavBar: React.FC<Props> = () => {
         </a>
       </Link>
 
-      <Link href="/auth/sign-in">
-        <a>
-          <Button>Sign In</Button>
-        </a>
-      </Link>
+      <div className="w-[720px] absolute left-1/2 -translate-x-1/2 flex items-center">
+        <MdSearch className="text-slate-400 mr-4" size={24} />
+        <input
+          className="font-sans text-sm placeholder-slate-400 text-slate-900 outline-none"
+          type="text"
+          placeholder="Search"
+          value={keyword}
+          onChange={(event) => setKeyword(event.target.value)}
+        />
+      </div>
+
+      {isLoggedIn && (
+        <div className="relative">
+          <button onClick={() => setIsDropdownOpen(!isDropdownOpen)}>
+            <img
+              className="w-10 h-10 rounded-full object-cover"
+              src="/images/dummy-avatar.png"
+              alt="John Doe"
+            />
+          </button>
+
+          {isDropdownOpen && <AccountDropdown />}
+        </div>
+      )}
+      {!isLoggedIn && (
+        <Link href="/auth/sign-in">
+          <a>
+            <Button>Sign In</Button>
+          </a>
+        </Link>
+      )}
     </header>
   );
 };
